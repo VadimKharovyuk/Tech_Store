e753e5bb6521ed90477cffcf375de948692330a0
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-08-01 01:11:04 +0300

    add home page

--- src/main/java/com/example/webservice/config/JacksonConfig.java
+++ src/main/java/com/example/webservice/config/JacksonConfig.java
@@ -1,11 +1,11 @@
-package com.example.webservice.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.context.annotation.Bean;
-
-public class JacksonConfig {
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
-    }
-}
+//package com.example.webservice.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import org.springframework.context.annotation.Bean;
+//
+//public class JacksonConfig {
+//    @Bean
+//    public ObjectMapper objectMapper() {
+//        return new ObjectMapper();
+//    }
+//}
---
+++ src/main/java/com/example/webservice/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.webservice.controller;
+
+import com.example.webservice.dto.Category;
+import com.example.webservice.repository.ProductFeignClient;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
+public class HomeController {
+    private final ProductFeignClient productFeignClient;
+
+
+    @GetMapping
+    public String homePage(Model model){
+       model.addAttribute("categories" ,productFeignClient.getAllCategories());
+        return "homePage";
+    }
+}
