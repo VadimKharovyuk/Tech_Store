f0c9018a9a9fa8543cf6c40a67cb07f1cb60353f
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-08-02 16:01:20 +0300

    accountUser

--- src/main/java/com/example/webservice/controller/AccountController.java
+++ src/main/java/com/example/webservice/controller/AccountController.java
@@ -1,20 +1,44 @@
 package com.example.webservice.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AccountController {
 
-    @GetMapping("/account")
-    public String accountUser(Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
+//    @GetMapping("/account")
+//    public String accountUser(Model model) {
+//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+//        String username = authentication.getName();
+//
+//        model.addAttribute("username", username);
+//        return "account/account";
+//    }
+@GetMapping("/account")
+public String accountUser(Model model) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    String username = authentication.getName();
+    boolean isAdmin = authentication.getAuthorities().stream()
+            .anyMatch(authority -> authority.getAuthority().equals("ROLE_ADMIN"));
 
-        model.addAttribute("username", username);
-        return "account/account";
+    model.addAttribute("username", username);
+    model.addAttribute("isAdmin", isAdmin);
+    return "account/account";
+}
+
+    @PostMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
     }
 }
--- src/main/java/com/example/webservice/dto/UserDTO.java
+++ src/main/java/com/example/webservice/dto/UserDTO.java
@@ -11,5 +11,4 @@ public class UserDTO {
     private String username;
     private String password;
     private String email;
- 
 }
